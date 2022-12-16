07841a66e27a906f78440ff7a9ee1bc1812b24f0
ProgrammerShahJalal <you@example.com>
2022-12-16 17:27:05 +0600

    solved quickly

---
+++ problem4/problem4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE * inputFile = fopen("input.txt", "r");
+    FILE * outputFile = fopen("output.txt", "w");
+    int n, last = 0, sum = 0;
+    fscanf(inputFile, "%d", &n);
+    int a[n + 5];
+    for (int i = 0; i < n; i++)
+    {
+        fscanf(inputFile, "%d", &a[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        last = a[i] % 10;
+        sum += last;
+    }
+
+    fprintf(outputFile, "Sum = %d\n", sum);
+    return 0;
+}
