86064d6fc363cf4e810e3cb3b3aae2be227839f4
ProgrammerShahJalal <you@example.com>
2022-12-16 18:01:55 +0600

    boom

--- problem2/problem2.c
+++ problem2/problem2.c
@@ -1,12 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    FILE * inputFile = fopen("input.txt", "r");
-    FILE * outputFile = fopen("output.txt", "w");
+    FILE *inputFile = fopen("input.txt", "r");
+    FILE *outputFile = fopen("output.txt", "w");
 
+    if (inputFile == NULL)
+    {
+        fprintf(outputFile, "inputFile not found\n");
+        return 0;
+    }
     int n;
     fscanf(inputFile, "%d", &n);
-    for(int i=1; i<=n ; i++){
+    for (int i = 1; i <= n; i++)
+    {
         fprintf(outputFile, "%d ", i);
     }
     return 0;
